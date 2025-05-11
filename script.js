acee4cadcf707f30a22d8eb9a29842b5acf66d28
hiteshkataria <36961088+hiteshkataria@users.noreply.github.com>
2025-05-11 19:29:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyZs7Xr2OxZdwMg-iZFJYaBDrj3ik5H-mkXKncQrMPyvPXvnfDmkWGP9O0eFpVxe_U1Xw/exec';
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw8EfEU_GbS1bEo0Z0xfR8m-kPgiJNxo70x979XaRFiKLk5S4F7giAXlfh_nc7tHkwjlA/exec';
+//const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw8EfEU_GbS1bEo0Z0xfR8m-kPgiJNxo70x979XaRFiKLk5S4F7giAXlfh_nc7tHkwjlA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbytgDdljUUSrcqx2E7RH3tk7AEYGUfmhbEzyTeud5tMa1IRC8RLD_OxrD66RKiHziyzYg/exec';
 
 window.onload = function () {
   // Show loading screen
