e27a0a4ea458312d7e875a0ddb57ab9246f01ba1
hiteshkataria <36961088+hiteshkataria@users.noreply.github.com>
2025-05-30 20:58:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const ENV_CONFIG = {
   SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbys-O_WgkmM8oy1Bned3DNc1wlynFItC2kqezOo6bg9c4ThSScnI4UQljDdwlvnj67YqQ/exec'
 };
 
-if (window.location.hostname.includes('dev') {
+if (window.location.hostname.includes('dev')) {
   ENV_CONFIG.SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwdtd7M7euMDRI8-S4OoImvHrDwQLYIBJ33Dgwv2ejnJEH6bwOUnHmMzgvxeni5U0Ia_A/exec';
 }
